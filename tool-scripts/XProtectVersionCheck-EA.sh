99c61d5aca9841726f2674e2c276b68aeb92a262
Graham Pugh <graham.pugh@jamf.com>
2024-04-25 12:41:47 +0200

    New EA for SU check

--- tool-scripts/XProtectVersionCheck-EA.sh
+++ tool-scripts/XProtectVersionCheck-EA.sh
@@ -2,24 +2,31 @@
 # shellcheck shell=bash
 
 ## based on Graham Pugh's version https://github.com/grahampugh/osx-scripts/blob/main/check-xprotect-version/XProtectVersionCheck-EA.sh
+#
+# Note that this uses plutil so is only compatible with macOS 12+
 
 # URL to the online JSON data
-onlineJsonUrl="https://macadmins.github.io/sofa/v1/macos_data_feed.json"
+online_json_url="https://macadmins.github.io/sofa/v1/macos_data_feed.json"
+json_data=$(/usr/bin/curl -s "$online_json_url")
+if [[ ! "$json_data" ]]; then
+    echo "<result>Could not obtain data</result>"
+    exit
+fi
 
 # Extract the online version of XProtect configuration data
-onlineXProtectVersion=$(curl -s "$onlineJsonUrl" | grep -o '"com.apple.XProtect": "[^"]*' | awk -F': "' '{print $2}')
+onlineXProtectVersion=$(/usr/bin/plutil -extract "XProtectPlistConfigData.com\\.apple\\.XProtect" raw - - <<< "$json_data" | /usr/bin/head -n 1)
 echo "Online XProtect Version: $onlineXProtectVersion"
 
-# Extract the online version of XProtect.app
-onlineXProtectAppVersion=$(curl -s "$onlineJsonUrl" | grep -o '"com.apple.XProtectFramework.XProtect": "[^"]*' | awk -F': "' '{print $2}')
-echo "Online XProtect.app Version: $onlineXProtectAppVersion"
-
 # Extract the local installed version of XProtect configuration data
-localXProtectVersion=$(defaults read /Library/Apple/System/Library/CoreServices/XProtect.bundle/Contents/Info.plist CFBundleShortVersionString)
+localXProtectVersion=$(/usr/bin/plutil -extract CFBundleShortVersionString raw /Library/Apple/System/Library/CoreServices/XProtect.bundle/Contents/Info.plist)
 echo "Local XProtect Version: $localXProtectVersion"
 
+# Extract the online version of XProtect.app
+onlineXProtectAppVersion=$(/usr/bin/plutil -extract "XProtectPayloads.com\\.apple\\.XProtectFramework\\.XProtect" raw - - <<< "$json_data" | /usr/bin/head -n 1)
+echo "Online XProtect.app Version: $onlineXProtectAppVersion"
+
 # Extract the local installed version of XProtect.app
-localXProtectAppVersion=$(defaults read /Library/Apple/System/Library/CoreServices/XProtect.app/Contents/Info.plist CFBundleShortVersionString)
+localXProtectAppVersion=$(/usr/bin/plutil -extract CFBundleShortVersionString raw /Library/Apple/System/Library/CoreServices/XProtect.app/Contents/Info.plist)
 echo "Local XProtect.app Version: $localXProtectAppVersion"
 
 # Compare the online version with the installed version for both XProtect and XProtect.app
