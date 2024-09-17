d9f62c78c4bafbb1cf08cbafd0a3f6c62d010bc7
Bart Reardon <bart.reardon@csiro.au>
2024-09-17 15:45:48 +1000

    Updated to use non symlink path as requested

--- tool-scripts/XProtectVersionCheck-EA.sh
+++ tool-scripts/XProtectVersionCheck-EA.sh
@@ -16,7 +16,7 @@ online_json_url="https://sofafeed.macadmins.io/v1/macos_data_feed.json"
 user_agent="SOFA-Jamf-EA-XProtectVersionCheck/1.1"
 
 # local store
-json_cache_dir="/var/tmp/sofa"
+json_cache_dir="/private/var/tmp/sofa"
 json_cache="$json_cache_dir/macos_data_feed.json"
 etag_cache="$json_cache_dir/macos_data_feed_etag.txt"
 etag_cache_temp="$json_cache_dir/macos_data_feed_etag_temp.txt"
